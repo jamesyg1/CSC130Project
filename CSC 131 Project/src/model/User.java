8f0788dfa1ba12c448496bb8392e41eca4bd25d8
lilja <lilja@LAPTOP-0DJUE87N>
2026-04-14 18:47:57 -0700

    Project Classes Commit

---
+++ CSC 131 Project/src/model/User.java
@@ -0,0 +1,38 @@
+package model;
+
+public class User {
+	
+	private int userID;
+	private String email;
+	private String password;
+	private String name;
+	
+	/* Constructor */
+	
+	public User(int userID, String email, String password, String name) {
+		
+		this.userID = userID;
+		this.email = email;
+		this.password = password;
+		this.name = name;
+	}
+	
+	/* Getter Methods */
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String password() {
+		return password;
+	}
+	
+	public String name() {
+		return name;
+	}
+
+}
