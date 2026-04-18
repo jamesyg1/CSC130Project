99465e058f0626b578aa0aa16b74bc74cf945eb3
izalesd <derick4546@gmail.com>
2026-04-17 19:43:53 -0700

    Created item class

--- CSC 131 Project/src/model/Item.java
+++ CSC 131 Project/src/model/Item.java
@@ -1,6 +1,29 @@
 package model;
 
 public class Item {
+	/*  Attributes  */
+	private String name;
+	private int count;
+	private double price;
 	
-
+	
+	/*  Constructor  */
+	public Item(String name, int count, double orice) {
+		this.name = name;
+		this.count = count;
+		this.price = price;
+	}
+	
+	
+	/*  Getter Methods */
+	public String getName() {
+		return name;
+	}
+	public int getCount() {
+		return count;
+	}
+	public double getPrice() {
+		return price;
+	}
+	
 }
--- CSC 131 Project/src/model/Receipt.java
+++ CSC 131 Project/src/model/Receipt.java
@@ -1,5 +1,8 @@
 package model;
+import java.util.Date;
 
 public class Receipt {
-
+	private Date date;
+	private User owner;
+	
 }
